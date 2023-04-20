b2ea2c15f08e6954e10dc6f935d4d32948b1ddb6
Daily-manjoo <yuslz35@gmail.com>
2023-04-21 02:47:26 +0900

    0421 commit

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 const todoInput = document.querySelector('#todo-input');
 
-const keyCodeCheck = function (){
-    if(window.event.keyCode === 13 && todoInput.value !== ''){ //13은 엔터
-        const todoList = document.querySelector('#todo-list');
-        const newLi = document.createElement('li');
-        const newSpan = document.createElement('span');
-        
+const createTodo = function (){
+    const todoList = document.querySelector('#todo-list');
+    const newLi = document.createElement('li');
+    const newSpan = document.createElement('span');
+    const newBtn = document.createElement('button');
+    
+    newBtn.addEventListener('click', ()=> {
+        newLi.classList.toggle('complete')
+    })
 
-        newSpan.textContent = todoInput.value;
-        newLi.appendChild(newSpan); //하위 노드 추가
-        todoList.appendChild(newLi);
-        todoList.appendChild(newLi);
-        todoInput.value = '';
+    newSpan.textContent = todoInput.value;
+    newLi.appendChild(newBtn);
+    newLi.appendChild(newSpan); //하위 노드 추가
+    todoList.appendChild(newLi);
+    console.log(newLi);
+    todoInput.value = '';
+}
+
+const keyCodeCheck = function (){
+    if(window.event.keyCode === 13 && todoInput.value){ //13은 엔터
+        createTodo();
+        
         
     }               
 }; 
