db501f7f752ed404182bc2477b899a5821c495de
Daily-manjoo <yuslz35@gmail.com>
2023-04-23 01:39:30 +0900

    0423 commit

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const todoInput = document.querySelector('#todo-input');
 const todoList = document.querySelector('#todo-list');
 
+const savedTodoList = JSON.parse(localStorage.getItem('saved-items')) //키 가져오기(스트링형태->객체형태)
 
-const createTodo = function (){
+
+
+const createTodo = function (storageData){
+    let todoContents = todoInput.value;
+    if (storageData){
+        todoContents = storageData.contents;
+    }
+
     const newLi = document.createElement('li');
     const newSpan = document.createElement('span');
     const newBtn = document.createElement('button');
@@ -17,7 +25,12 @@ const createTodo = function (){
         
     });
 
-    newSpan.textContent = todoInput.value;
+    if(storageData.complete) {
+        newLi.classList.add('complete'); //취소선 그어진 목록 그대로 반영 
+    }
+
+
+    newSpan.textContent = todoContents;
     newLi.appendChild(newBtn);
     newLi.appendChild(newSpan); //하위 노드 추가
     todoList.appendChild(newLi);
@@ -52,7 +65,13 @@ const saveItemsFn = function (){
         saveItems.push(todoObj);
     }
 
-    localStorage.setItem('saved-items', JSON.stringify(saveItem)); //키,밸류
+    localStorage.setItem('saved-items', JSON.stringify(saveItems)); //키,밸류
     
+}
+
+if (savedTodoList) {
+    for(let i = 0; i <savedTodoList.length; i++){
+        createTodo(savedTodoList[i])
+    }
 }
     
